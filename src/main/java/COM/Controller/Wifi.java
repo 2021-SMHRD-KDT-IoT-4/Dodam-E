88c345aba546dfe5c97d42ecfa3d8a92f62a6a26
smhrd <smhrd@DESKTOP-6IHAUI2>
2021-06-28 15:55:25 +0900

    ghkdtnqh커밋

--- src/main/java/COM/Controller/Wifi.java
+++ src/main/java/COM/Controller/Wifi.java
@@ -17,52 +17,53 @@ import COM.Model.RouteDAO;
 import COM.Model.RouteDTO;
 import COM.Model.SendDTO;
 import COM.Model.UserDTO;
+import COM.Model.childDAO;
 import COM.Model.childDTO;
 
 @WebServlet("/Wifi")
 public class Wifi extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		request.setCharacterEncoding("EUC-KR");
-//		Integer.parseInt(request.getParameter("sb"));
-		int js =1;
+   protected void service(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      request.setCharacterEncoding("EUC-KR");
+//      Integer.parseInt(request.getParameter("sb"));
+      int js = 1;
 
-		System.out.println(js);
+      System.out.println(js);
 
-		RouteDAO dao = new RouteDAO();
-		ArrayList<ReceiveDTO> list = dao.loca();
-		int num = 0;
-		String loca = "";
-		HttpSession session = request.getSession();
-		UserDTO info = (UserDTO) session.getAttribute("login_info");
-//		String mcr = (String) session.getAttribute("mcr");
-		System.out.println(info.getId());
-//		String cc = (String) session.getAttribute("c_name");
-//		System.out.println(cc);
+      RouteDAO dao = new RouteDAO();
+      ArrayList<ReceiveDTO> list = dao.loca();
+      int num = 0;
+      String loca = "";
+//      HttpSession session = request.getSession();
+//      UserDTO info = (UserDTO) session.getAttribute("login_info");
+//      String mcr = (String) session.getAttribute("mcr");
+//      System.out.println(info.getId());
+//      String cc = (String) session.getAttribute("c_name");
+//      System.out.println(cc);
 
+//         String id = info.getId();
+      childDAO user = new childDAO();
+      ArrayList<childDTO> dto = user.Child_one_info("ss");
+      if (js > 0) {
 
-		if (info != null) {
-			String id = info.getId();
-			
-			if (js > 0) {
-				if (id.equals("ss")) {
+         System.out.println(list.size());
+         for (int i = 0; i <list.size() ; i++) {
+            num = list.get(i).getReceive_num();
 
-					for (int i = 0; i < list.size(); i++) {
-						num = list.get(i).getReceive_num();
+            if (num == js) {
+               if (dto.get(i).getC_send_machine().equals("11")) {
 
-						if (num == js) {
-							// 아이이름,유저아이디,센드번호,
-							loca = list.get(i).getReceive_loca();
-							 dao.upload(info, "황수보", loca);
-							
-						}
-					}
-				}
+                  loca = list.get(i).getReceive_loca();
+                  dao.upload("ss",dto.get(i).getC_name() , loca);
+                  System.out.println(dto.get(i).getC_name());
+                  break;
+               }
+            }
+         }
 
-			}
-		}
+      }
 
-	}
+   }
 }
